1ebfc270ad5ac3db035d834a0affde6031c8a119
Mikel Soria Jimenez <msoria-j@c4r6s1.42urduliz.com>
2023-10-13 15:15:30 +0200

    starting cpp00_ex01

---
+++ cpp_module_00/ex01/PhoneBook.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msoria-j <msoria-j@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 15:08:30 by msoria-j          #+#    #+#             */
+/*   Updated: 2023/10/13 15:14:15 by msoria-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(void) {
+	index = 0;
+	std::cout << "Constructor, index: " << index << std::endl;
+}
+
+PhoneBook::~PhoneBook(void) {
+	
+}
---
+++ cpp_module_00/ex01/PhoneBook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msoria-j <msoria-j@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 14:25:54 by msoria-j          #+#    #+#             */
+/*   Updated: 2023/10/13 15:10:29 by msoria-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
+# include <iostream>
+# include <iomanip>
+
+class PhoneBook {
+	
+private:
+	/* data */
+	int	index;
+public:
+	PhoneBook(/* args */);
+	~PhoneBook();
+};
+
+
+#endif /* PHONE_BOOK_HPP */

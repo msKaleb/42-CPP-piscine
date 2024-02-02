4b305b9fb5c3658a9d4adcf5a7deb943b53bf342
msKaleb <sjmikel@gmx.es>
2024-02-02 08:00:34 +0100

    cpp0503 makeForm()

--- cpp_module_05/ex03/Intern.cpp
+++ cpp_module_05/ex03/Intern.cpp
@@ -23,3 +23,38 @@ Intern	&Intern::operator=(const Intern &rhs) {
 	}
 	return (*this);
 }
+
+AForm	*Intern::shrubbery(std::string const &formTarget) {
+	return new ShrubberyCreationForm(formTarget);
+}
+
+AForm	*Intern::robotomy(std::string const &formTarget) {
+	return new RobotomyRequestForm(formTarget);
+}
+
+AForm	*Intern::presidential(std::string const &formTarget) {
+	return new PresidentialPardonForm(formTarget);
+}
+
+AForm	*Intern::makeForm(std::string const &formName, std::string const &formTarget) {
+	// string of available forms
+	std::string	formTypes[] = {
+		std::string("shrubbery creation"),
+		std::string("robotomy request"),
+		std::string("presidential pardon")
+	};
+	// pointer to functions array
+	AForm	*(Intern::*funct[])(std::string const &target) = {
+		&Intern::shrubbery,
+		&Intern::robotomy,
+		&Intern::presidential
+	};
+
+	for (int i = 0; i < 3; i++) {
+		if (formName == formTypes[i]) {
+			return ((this->*funct[i])(formTarget));
+		}
+	}
+	// try-catch block?
+	return NULL;
+}
--- cpp_module_05/ex03/Intern.hpp
+++ cpp_module_05/ex03/Intern.hpp
@@ -2,9 +2,14 @@
 # define INTERN_HPP
 
 # include <iostream>
-# include "AForm.hpp"
+# include "ShrubberyCreationForm.hpp"
+# include "RobotomyRequestForm.hpp"
+# include "PresidentialPardonForm.hpp"
 
 class	AForm;
+class	ShrubberyCreationForm;
+class	RobotomyRequestForm;
+class	PresidentialPardonForm;
 
 class	Intern
 {
@@ -19,6 +24,10 @@ public:
 	Intern &operator=(const Intern &rhs);
 
 	AForm	*makeForm(std::string const &formName, std::string const &formTarget);
+
+	AForm	*shrubbery(std::string const &formTarget);
+	AForm	*robotomy(std::string const &formTarget);
+	AForm	*presidential(std::string const &formTarget);
 };
 
 #endif /* INTERN_HPP */

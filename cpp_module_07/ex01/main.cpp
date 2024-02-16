721b8e5dbe107fd62315391816869fa053c6bb7c
msKaleb <sjmikel@gmx.es>
2024-02-16 17:14:17 +0100

    Fri Feb 16 17:14:17 CET 2024

--- cpp_module_07/ex01/main.cpp
+++ cpp_module_07/ex01/main.cpp
@@ -1,18 +1,21 @@
 #include "iter.hpp"
-// #include "../ex00/functions.hpp"
 
-/**
- * @todo create an array of functions with return value
- */
+template<typename T>
+void	print(T element) {
+	std::cout << "print: " << element << std::endl;
+
+	return ;
+}
+
 int main() {
-    int a[5] = {1, 3, 5, 7, 9};
+	int a[5] = {1, 3, 5, 7, 9};
 
-    ::iter(a, 5, ::print);
+	::iter(a, 5, ::print);
 
-    std::string str[3] = {"Mikel", "Soria", "Jiménez"};
-    ::iter(str, 3, ::print);
+	std::string str[] = {"Fee", "Fie", "Foe", "Foo"};
+	::iter(str, 4, ::print);
 
-    float   f[7] = {3.14f, 5.43f, 6, 87.23f, 12.5f, 11, 34.76f};
-    ::iter(f, 7, ::print);
-    return 0;
+	float   f[7] = {3.14f, 5.43f, 6, 87.23f, 12.5f, 11, 34.76f};
+	::iter(f, 7, ::print);
+	return 0;
 }
---
+++ cpp_module_07/ex02/Array.hpp
@@ -0,0 +1,24 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+template<typename T>
+class Array
+{
+private:
+	/* data */
+	Array();
+public:
+	Array(int elements);
+	~Array();
+};
+
+Array::Array(/* args */)
+{
+}
+
+Array::~Array()
+{
+}
+
+
+#endif /* ARRAY_HPP */

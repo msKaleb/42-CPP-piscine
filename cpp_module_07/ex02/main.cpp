aa706e5ceb0ca3070f5169fab400abec175fa5cf
Mikel Soria Jimenez <msoria-j@c4r8s6.42urduliz.com>
2024-02-17 16:48:21 +0100

    Sat Feb 17 16:48:21 CET 2024

--- cpp_module_07/ex02/main.cpp
+++ cpp_module_07/ex02/main.cpp
@@ -2,8 +2,20 @@
 #include "Array.hpp"
 
 #define MAX_VAL 750
+
 int main(int, char**)
 {
+    Array<std::string> myStrings(5);
+    myStrings[0] = "Consectetur eiusmod elit est sunt culpa laboris cillum labore id deserunt";
+    myStrings[1] = "cupidatat culpa velit id. Duis excepteur consequat proident";
+    myStrings[2] = "incididunt et elit duis irure. Fugiat enim velit cillum labore";
+    myStrings[3] = "reprehenderit ea cillum id. Irure non fugiat eu occaecat";
+    myStrings[4] = "commodo sit commodo tempor. Mollit dolor reprehenderit nisi voluptate incididunt.";
+    std::cout << "Size of myStrings: " << myStrings.size() << std::endl;
+    for (int i = 0; i < 5; i++)
+        std::cout << myStrings[i] << std::endl;
+    
+    std::cout << std::endl;
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
@@ -12,14 +24,11 @@ int main(int, char**)
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
-        std::cout << numbers[i] << std::endl;
     }
-        std::cout << std::endl;
     //SCOPE
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
-        // Array<int> test(numbers);
     }
 
     for (int i = 0; i < MAX_VAL; i++)
@@ -50,7 +59,6 @@ int main(int, char**)
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
-        std::cout << numbers[i] << std::endl;
     }
     delete [] mirror;//
     return 0;

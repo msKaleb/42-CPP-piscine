32ef5bc6b377f1c0cb6cd4cff48c96c2d9c63b06
msKaleb <sjmikel@gmx.es>
2024-02-27 08:11:43 +0100

    Tue Feb 27 08:11:43 CET 2024

--- cpp_module_08/ex02/MutantStack.hpp
+++ cpp_module_08/ex02/MutantStack.hpp
@@ -18,16 +18,26 @@ public:
 	/* orthodox canonical */
 	MutantStack<T, Container>() : std::stack<T>() {}
 	~MutantStack<T, Container>() {};
-	MutantStack<T, Container>(MutantStack<T, Container> const& copy) { *this = copy; }
+	MutantStack<T, Container>(MutantStack<T, Container> const& copy) : std::stack<T>(copy) {/*  *this = copy;  */}
 	MutantStack<T, Container> &operator=(const MutantStack<T, Container> &rhs) {
 		if (this != &rhs)
-			this->c = rhs->c;
+			this->c = rhs.c;
 		return *this;
 	}
 
-	typedef typename Container::iterator iterator;
-	iterator	begin() { return this->c.begin(); }
-	iterator	end() { return this->c.end(); }
+	typedef typename Container::const_reverse_iterator	const_reverse_iterator;
+	typedef typename Container::reverse_iterator		reverse_iterator;
+	typedef typename Container::const_iterator			const_iterator;
+	typedef typename Container::iterator				iterator;
+
+	iterator				begin() 		{ return this->c.begin(); }
+	iterator				end() 			{ return this->c.end(); }
+	const_iterator			cbegin() const 	{ return this->c.begin(); }
+	const_iterator			cend() const 	{ return this->c.end(); }
+	reverse_iterator		rbegin() 		{ return this->c.rbegin(); }
+	reverse_iterator		rend() 			{ return this->c.rend(); }
+	const_reverse_iterator	rbegin() const	{ return this->c.rbegin(); }
+	const_reverse_iterator	rend() const	{ return this->c.rend(); }
 };
 
 #endif /* MUTANTSTACK_HPP */
--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -1,7 +1,39 @@
 #include <list>
+#include <cstdlib>
 #include "MutantStack.hpp"
 
 int main() {
+/* { // subject example
+	MutantStack<int>	mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+} */
 	MutantStack<int>	mStack;
 	std::list<int>		lst;
 	std::deque<int>		dc;
@@ -12,27 +44,45 @@ int main() {
 		dc.push_back(i * i);
 	}
 
-	std::cout << mStack.top() << std::endl;
-	MutantStack<int>::iterator	it = mStack.begin();
-	MutantStack<int>::iterator	ite = mStack.end();
-	std::list<int>::iterator	lit = lst.begin();
+ // iterators ******************************************************************
+	MutantStack<int>::iterator	mIt = mStack.begin();
+	MutantStack<int>::iterator	mIte = mStack.end();
+	std::list<int>::iterator	lIt = lst.begin();
 	std::list<int>::iterator	lite = lst.end();
-	std::deque<int>::iterator	dit = dc.begin();
-	std::deque<int>::iterator	dite = dc.end();
+	std::deque<int>::iterator	dIt = dc.begin();
+	std::deque<int>::iterator	dIte = dc.end();
 
-	std::cout << "it: " << *it << std::endl;
-	std::cout << "ite: " << *ite << std::endl;
-	std::cout << "dit: " << *dit << std::endl;
-	std::cout << "dite: " << *dite << std::endl;
-	std::cout << "lit: " << *lit << std::endl;
-	std::cout << "lite: " << *lite << std::endl;
+	/* std::cout << "mIt: " << *mIt << std::endl;
+	std::cout << "mIte: " << *mIte << std::endl;
+	std::cout << "dIt: " << *dIt << std::endl;
+	std::cout << "dIte: " << *dIte << std::endl;
+	std::cout << "lIt: " << *lIt << std::endl;
+	std::cout << "lite: " << *lite << std::endl; */
 
-	while (it != ite)
-		std::cout << "mStack: " << *it++ << std::endl;
-	while (lit != lite)
-		std::cout << "lst: " << *lit++ << std::endl;
-	while (dit != dite)
-		std::cout << "dc: " << *dit++ << std::endl;
+ // loop over to compare *******************************************************
+	while (mIt != mIte)
+		std::cout << "MutantStack: " << *mIt++ << std::endl;
+	while (lIt != lite)
+		std::cout << "list: " << *lIt++ << std::endl;
+	while (dIt != dIte)
+		std::cout << "deque: " << *dIt++ << std::endl;
+
+ // testing copy constructor ***************************************************
+	MutantStack<int>	copy(mStack);
+
+	MutantStack<int>::const_iterator	cIt = copy.cbegin();
+	MutantStack<int>::const_iterator	cIte = copy.cend();
+	while (cIt != cIte)
+		std::cout << "copy: " << *cIt++ << std::endl;
+
+ // testing assignation operator ***********************************************
+ 	MutantStack<int>	eq;
+
+	eq = copy;
+	MutantStack<int>::reverse_iterator	eIt = eq.rbegin();
+	MutantStack<int>::reverse_iterator	eIte = eq.rend();
+	while (eIt != eIte)
+		std::cout << "eq: " << *eIt++ << std::endl;
 
 	return (0);
 }

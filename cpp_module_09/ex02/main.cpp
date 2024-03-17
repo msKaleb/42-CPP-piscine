8442d72b77a1f536e43981e60ef86a7d90c2d8b8
msKaleb <sjmikel@gmx.es>
2024-03-17 14:46:36 +0100

    Sun Mar 17 14:46:36 CET 2024

--- cpp_module_09/ex02/main.cpp
+++ cpp_module_09/ex02/main.cpp
@@ -1,8 +1,34 @@
 #include "PmergeMe.hpp"
+#include <cstdlib>
+#include <sstream>
 
-int main() {
-	//	Your Code
+size_t	jacobsthal(size_t n) {
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return jacobsthal(n - 1) + 2 * (jacobsthal(n - 2));
+}
+int main(int argc, char* argv[]) {
+	std::string	numbers = "";
+	std::string	item;
+	size_t		size = 0;
 
+	for (int i = 1; i < argc; i++) {
+		if (std::strtol(argv[i], NULL, 10) < 0) {
+			std::cout << "Only positive integers" << std::endl;
+			return 1;
+		}
+		numbers += argv[i];
+		numbers += " ";
+		size++;
+	}
+	std::stringstream	ss(numbers);
+	// int*	chain = new int[size / 2];
+	while (ss >> item) {
+		std::cout << item << std::endl;
+		
+	}
 
 	return (0);
 }

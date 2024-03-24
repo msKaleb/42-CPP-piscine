ac548982cbb4e4cb0813694290d724f76234f356
Mikel Soria Jimenez <msoria-j@c3r3s6.42urduliz.com>
2024-03-24 11:18:39 +0100

    finished

--- cpp_module_09/ex01/main.cpp
+++ cpp_module_09/ex01/main.cpp
@@ -1,12 +1,19 @@
 #include "RPN.hpp"
 
 int main(int argc, char* argv[]) {
-	if (argc != 2)
+	if (argc < 2) {
+		std::cout << "Input needed" << std::endl;
 		return 1;
+	}
 
-	std::string	exp(argv[1]);
+	// get the whole expression
+	std::string	exp = "";
+	for (int i = 1; i < argc; i++) {
+		exp += argv[i];
+		exp += " ";
+	}
+
 	RPN			rpn;
-
 	try {
 		rpn.parseExpresion(exp);
 		std::cout << "RPN: " << rpn.compute() << std::endl;
